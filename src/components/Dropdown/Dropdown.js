65e37b059415153068421307f254a274708089ac
Sofia <sofiareis2018@gmail.com>
2023-12-12 14:17:52 -0800

    Feat: Add Dropdown for user options
    - Add a dropdown component

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import './Dropdown.css';
+
+
+function Dropdown({data, onDataSelected, type}) {
+  const [item, setItem] = useState([]); 
+  const [selectItem, setSelectItem] = useState([]);
+  const [isDropdownItemOpen, setIsDropdownItemOpen] = useState(false);
+
+  useEffect(() =>{
+    setItem(data)
+  }, [data])
+
+
+  const toggleDropdownItem = () => {
+    setIsDropdownItemOpen(!isDropdownItemOpen);
+  };
+
+  const handleColumnItemSelect = (e) => {
+    const selectedOptions = parseInt(e.target.value);
+  
+    if (selectedOptions === -1) {
+      setSelectItem([...item.keys()]);
+      onDataSelected && onDataSelected([...item.keys()]);
+
+    } else {
+      setSelectItem([selectedOptions]);
+      onDataSelected && onDataSelected([selectedOptions]);
+    }
+  };
+
+  const handleDropdownItem = (index) => {
+      setSelectItem([index]);
+      onDataSelected && onDataSelected([index]); 
+  };
+
+  const isItemSelected = (index) => selectItem.includes(index);
+  
+  return (
+    <div>
+        <label className='dropdown-label'>
+          Select over which item to display:
+          <div className={`dropdown-custom-dropdown ${isDropdownItemOpen ? 'open' : ''}`}>
+            <div className="selected-values" onClick={toggleDropdownItem}>
+              {selectItem.length === item.length
+                ? 'Over all items'
+                : selectItem.map((index) => item[index])}
+            </div>
+            {isDropdownItemOpen && (
+              <div className="dropdown-options">
+                <div
+                className={isItemSelected(-1) ? 'selected' : ''}
+                  key="-1"
+                  onClick={() => handleColumnItemSelect({ target: { value: -1 } })}
+                >
+                  Over all items
+                </div>
+                {item.map((column, index) => (
+                  <div 
+                    key={index} 
+                    className={isItemSelected(index) ? 'selected' : ''}
+                    onClick={() => handleDropdownItem(index)}>
+                  {column}
+                </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </label>
+    </div>  
+  );
+}
+
+export default Dropdown;
